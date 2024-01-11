e810bf394d794177015b3c948908559989da9dcc
Garvit Bibra <garvitbibra@outlook.com>
2024-01-11 15:58:36 -0600

    Implemented a Sine Oscillator class for audio frequency

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#include <cmath>
+
+
+const int SAMPLE_RATE = 44100;
 
 class SineOscillator{
 private:
-	float frequency, amplitude;
+	float frequency, amplitude, angle = 0.0f, offset = 0.0f;
 public:
-	SineOscillator(float freq, float amp): frequency(freq), amplitude(amp) {}
-	float process(){
+	SineOscillator(float freq, float amp): frequency(freq), amplitude(amp) {
+		offset = 2 * M_PI * frequency / SAMPLE_RATE;
+	}
 
+	float process(){
+		auto sample = amplitude * sin(angle);     // A * sin(2 * pi * f / sr)
+		angle += offset;
+		return sample;
 	}
 };
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	SineOscillator sosc(440, 0.5);
 	return 0;
 }
